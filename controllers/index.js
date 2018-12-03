2ccdb615649f76001eb616a74fff80d531ad0888
Eden <1059951183@qq.com>
2018-12-03 16:47:40 +0800

    x

---
+++ controllers/index.js
@@ -0,0 +1,24 @@
+var index = async(ctx,next)=>{
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;   
+}
+var signin =async(ctx,next)=>{
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+}
+module.exports = {
+    'GET /':index,
+    'POST /signin':signin
+}//将两个url处理函数暴露出来
