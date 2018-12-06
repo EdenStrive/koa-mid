01c5bb3c6c87333ab9758e77b72575a1324bcbf7
Eden <18166044122@163.com>
2018-12-06 21:03:47 +0800

    x

--- controllers/hello.js
+++ controllers/hello.js
@@ -1,6 +1,7 @@
 var hello = async(ctx,next)=>{
     var name = ctx.params.name;
     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+    console.log(ctx)
 }
 module.exports={
     'GET /hello/:name':hello
